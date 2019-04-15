a06abe687949e294777f1702f70fdf57641df46e
李绍良 <46514593+lishaoliang@users.noreply.github.com>
2019-04-15 15:57:22 +0800

    广播发现设备

--- demo/demo/nspp/t_discover.lua
+++ demo/demo/nspp/t_discover.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2019 武汉舜立软件, All Rights Reserved
 -- Created: 2019/3/6
 --
--- @brief	测试组播搜索设备
+-- @brief	测试网络发现设备
 -- @author	李绍良
 -- @see https://github.com/lishaoliang/l_sdk_doc/blob/master/multicast/multicast.md
 --]]
@@ -14,31 +14,31 @@ local l_sdk = require("l_sdk")
 l_sdk.init('')
 
 
--- 打开组播设备
-l_sdk.multicast_open()
+-- 打开网络发现服务客户端
+l_sdk.discover_open('')
 
 
--- 启用组播搜索
-l_sdk.multicast_discover(true)
+-- 启用发现
+l_sdk.discover_run(true)
 
 
 local count = 11
 while 0 < count do
 	count = count - 1
 
-	local devs = l_sdk.multicast_get_devs()
-	print('multicast get devs:', devs)
+	local devs = l_sdk.discover_get_devs()
+	print('discover get devs:', devs)
 
 	l_sys.sleep(1000)
 end
 
 
--- 关闭组播搜索
-l_sdk.multicast_discover(false)
+-- 关闭发现
+l_sdk.discover_run(false)
 
 
--- 关闭组播设备
-l_sdk.multicast_close()
+-- 关闭网络发现服务客户端
+l_sdk.discover_close()
 
 -- sdk退出
 l_sdk.quit()
