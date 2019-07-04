b3500bf4b4d6685bb1e2cacaa55f674870df0b65
李绍良 <46514593+lishaoliang@users.noreply.github.com>
2019-07-04 16:31:37 +0800

    安卓示例

---
+++ demo/cpp/NdkAndroid/app/src/main/java/com/afscope/ndkandroid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.afscope.ndkandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        Log.i("mainactivity", "onCreate: "+Test());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String Test();
+}
